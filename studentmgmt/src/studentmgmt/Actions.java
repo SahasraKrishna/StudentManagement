1f4d2159361df5c445d8660c9a14ca2d4ba8fae6
Sahasra <Sahasra@DESKTOP-RD94PA5>
2024-06-18 19:27:57 +0530

    nxt commit

--- studentmgmt/src/studentmgmt/Actions.java
+++ studentmgmt/src/studentmgmt/Actions.java
@@ -4,10 +4,10 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.Scanner;
+//import java.util.Scanner;
 
 public class Actions {
-	// Scanner sc = new Scanner(System.in);
+	// Scanner so = new Scanner(System.in);
 	ArrayList<Student> students = new ArrayList<>();
 
 	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -26,44 +26,66 @@ public class Actions {
 			System.out.println(k);
 	}
 
+	/**
+	 * It adds the student details into the arraylist
+	 * 
+	 * @return returns nothing
+	 * @exception the
+	 *                name has to be of finite length also name consists of only
+	 *                alphabets but no numbers in it
+	 * 
+	 */
 	void addStudent() {
 		String name = readName();
-
 		System.out.print("enter the age:");
 		int age = readAge();
 		System.out.print("enter the gender:");
-		String gender="M";
+		String gender = "M";
 		try {
 			gender = br.readLine();
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
+			// TODO Auto-generated catch bloc
 			e.printStackTrace();
 		}
 		System.out.print("enter the grade:");
-//		int gr = sc.nextInt();
+		// int gr = sc.nextInt();
 		int gr = 44;
 		Student l = new Student(age, name, gender, gr);
 		students.add(l);
 	}
 
+	/**
+	 * used to store the value of the age of the student
+	 * 
+	 * @return age of student
+	 * @exception NumberFormat
+	 *                Exception
+	 */
+
 	private int readAge() {
 		int result = 0;
 		try {
 			System.out.print("enter the age:");
-//			return sc.nextInt();
+			// return sc.nextInt();
 			String tmp = br.readLine();
 			result = Integer.parseInt(tmp);
 		} catch (Exception e) {
 			System.out.println("\nInvalid number,  Enter number values for Age");
 			System.out.println(e.getMessage());
-//			sc.reset();
+			// sc.reset();
 			result = readAge();
 		}
 		return result;
 	}
 
+	/**
+	 * it is used to store the name of the student
+	 * 
+	 * @return name of student @exception
+	 */
+
 	private String readName() {
-		String name=null;
+		String name = null;
 		do {
 			System.out.println("enter the name:");
 			// name = sc.nextLine();
@@ -82,33 +104,49 @@ public class Actions {
 		return name;
 	}
 
+	/**
+	* 
+	*/
 	void deleteStudent() {
-		System.out.print("enter the regno:");
-//		int gr1 = sc.nextInt();
-//		students.removeIf(s -> s.getRegno() == gr1);
+
+		int gr1 = readRegno();
+		// students.removeIf(s -> s.getRegno() == gr1);
+	}
+
+	private int readRegno() {
+		int regno = 0;
+		try {
+			System.out.print("enter the regno:");
+			String tmp = br.readLine();
+			regno = Integer.parseInt(tmp);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return regno;
+
 	}
 
 	void modifyStudent() {
 		System.out.println("enter the regno of student to be modified:");
-//		int regno1 = sc.nextInt();
-//		sc.nextLine();
-//		for (Student s1 : students) {
-//			if (s1.getRegno() == regno1) {
-//				System.out.print("enter the age:");
-//				int age = sc.nextInt();
-//				sc.nextLine();
-//				System.out.print("enter the name:");
-//				String name1 = sc.nextLine();
-//				System.out.print("enter the gender:");
-//				String gender = sc.nextLine();
-//				System.out.print("enter the grade:");
-//				int gr = sc.nextInt();
-//				s1.setAge(age);
-//				s1.setName(name1);
-//				s1.setGender(gender);
-//				s1.setGrade(gr);
-//			}
+		// int regno1 = sc.nextInt();
+		// sc.nextLine();
+		// for (Student s1 : students) {
+		// if (s1.getRegno() == regno1) {
+		// System.out.print("enter the age:");
+		// int age = sc.nextInt();
+		// sc.nextLine();
+		// System.out.print("enter the name:");
+		// String name1 = sc.nextLine();
+		// System.out.print("enter the gender:");
+		// String gender = sc.nextLine();
+		// System.out.print("enter the grade:");
+		// int gr = sc.nextInt();
+		// s1.setAge(age);
+		// s1.setName(name1);
+		// s1.setGender(gender);
+		// s1.setGrade(gr);
+		// }
 
-//		}
+		// }
 	}
 }
